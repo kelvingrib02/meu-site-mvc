eee1ee6641e425955faa4144c243bb377fd0f4a6
Kelvin Gabriel  Ribeiro <kelvin@twrt.com.br>
2023-09-11 20:30:53 -0300

    Atualizando

---
+++ OnzeNove/meu-site-mvc/meu-site-mvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using meu_site_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace meu_site_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OnzeNove/meu-site-mvc/meu-site-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace meu_site_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
